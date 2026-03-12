8d65dc43329692c10b817f0dc18881778080e62e
Chris McKenzie <kristopolous@yahoo.com>
2026-03-12 11:50:45 -0700

    osx compatibility for the conversation.sh

--- examples/conversation.sh
+++ examples/conversation.sh
@@ -2,7 +2,7 @@
 conv=${CONV:-$(mktemp)}
 echo -e "  Using: $conv\n"
 jq -r '.[] | "\n**\(.role)**: \(.content)"' $conv | tail -100 | sd
-while read -E -p "  >> " query; do 
-    llcat -c $conv "$@" "$query" |& sd
+while builtin read -p "  >> " query; do 
+    llcat -c $conv "$@" "$query" | sd
     echo
 done
