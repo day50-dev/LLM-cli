6f80454ea1b5632a63483a97fa06e84facd52584
Chris McKenzie <kristopolous@yahoo.com>
2026-08-19 09:35:13 -0700

    toolcalling stylization

--- examples/conversation.sh
+++ examples/conversation.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
+
+# This isn't necessary, it's just to stylize the output of tollcalling
+function stream_parser() {
+	{
+    jq -r '
+        if .class == "toolcall" and .message == "request"
+        then
+            "**" + .obj.function.name + ":**\n\n```bash"
+            + (.obj.function.arguments | fromjson | to_entries
+               | map("  \(.key): \(.value)") | join("\n")) +
+						"\n```\n"
+        elif .message == "result"
+        then .obj.content[0].text
+        else empty
+        end'
+	} | sd
+}
+
 conv=${CONV:-$(mktemp)}
+
 echo -e "  Using: $conv\n"
 jq -r '.[] | "\n**\(.role)**: \(.content)"' $conv | tail -100 | sd
 while builtin read -p "  >> " query; do 
-    llcat -c $conv "$@" "$query" | sd
-    echo
+  llcat -c $conv "$@" "$query" 2> >(stream_parser) | sd
+  echo
 done
